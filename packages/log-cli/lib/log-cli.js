6b67dda62fce307294ffdfa145c4a564e4f1017a
jasper.nam <jasper.nam@kakaoenterprise.com>
2021-09-21 23:30:47 +0900

    ew

--- packages/log-cli/lib/log-cli.js
+++ packages/log-cli/lib/log-cli.js
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 
 const { program } = require('commander')
-const LogCore = require('@overpowerman/log-core')
+const LogCore = require('overpowerman-log-core')
 
 // action
 program.action(cmd => LogCore())
