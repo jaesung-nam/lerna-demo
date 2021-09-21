e8221b9520de94014161adbb7543ac8362968912
jasper.nam <jasper.nam@kakaoenterprise.com>
2021-09-22 00:22:28 +0900

    ewewew2222

--- packages/log-cli/lib/log-cli.js
+++ packages/log-cli/lib/log-cli.js
@@ -2,7 +2,7 @@
 
 const { program } = require('commander')
 const LogCore = require('overpowerr-log-core')
-console.log("e2222")
+console.log("e2222ewewe")
 // action
 program.action(cmd => LogCore())
 
